883d79944f630394566a4755353f74dc38e5e8e1
Eduardo Hideo Kutomi <dujpn01@gmail.com>
2021-06-14 22:17:00 -0300

    Criacao de classe para chamar API
    Criacao de classe para usar API e variaveis para manipular a tela

--- CriptoOrg/CriptoOrg/force-app/main/default/aura/TradeCmp/TradeCmpController.js
+++ CriptoOrg/CriptoOrg/force-app/main/default/aura/TradeCmp/TradeCmpController.js
@@ -1,5 +1,28 @@
 ({
-    myAction : function(component, event, helper) {
+    retrievePrice : function(component, event, helper) {
+        var action = component.get("c.getPrice");
+        action.setParams({
+            "mercadoId" : component.get("v.recordId")
+        });
 
-    }
+        action.setCallback(this, function(response){
+            var state = response.getState(); //Pega o retorno do response
+            
+            if(state == "SUCCESS"){
+                console.log(response.getReturnValue()); //Pega o valor
+            }
+            else{
+                console.log("Erro: " + JSON.stringify(response.getError()));
+            }
+        });
+
+        $A.enqueueAction(action); //Dispara a ação
+    },
+    handleOrdem : function(component, event, helper) {
+        alert("Codigo da ordem");
+    },
+    handleChange : function(component, event, helper) {        
+        console.log(event.getParam("checked"));
+        component.set('v.opMercado', !event.getParam("checked")); /*Seta valor na view*/
+    },
 })
